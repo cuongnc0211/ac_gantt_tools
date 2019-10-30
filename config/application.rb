a30f342eb4d1a9f0394b522850ebadebdeba3233
cuongnc0211 <cuongnguyenfu@gmail.com>
2019-10-30 22:38:22 +0700

    asset complie

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,8 @@ module AcGanttTool
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.assets.initialize_on_precompile = false
+    
     config.time_zone = 'Hanoi'
     config.active_record.default_timezone = :local
   end
