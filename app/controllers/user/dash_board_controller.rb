25318000f689fb30bed2ea1f1dd0e805597c147b
cuongnc0211 <cuongnguyenfu@gmail.com>
2020-01-13 17:42:52 +0700

    hot_fix_task_index

--- app/controllers/user/dash_board_controller.rb
+++ app/controllers/user/dash_board_controller.rb
@@ -1,6 +1,5 @@
 class User::DashBoardController < User::BaseController
   def index
-    @task = Task.first
     load_dates
     @projects = Project.all
     @tasks = Task.in_quater(@start_date, @end_date)
